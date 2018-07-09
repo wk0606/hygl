2651878693a6ec8dd168e4dee55481bf801e1f4d
wangkun <wkyhy@qq.com>
2018-07-09 09:15:52 +0800

    2

---
+++ src/views/mall/set/set_dp/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="set-tab">
+      <div
+        class="set-tab-item"
+        v-for="item in tabs"
+        :key="item.component"
+        :class="{'set-tab-select':item.component==currentTab}"
+        @click="currentTab=item.component"
+      >{{item.label}}</div>
+    </div>
+    <div class="set-body">
+      <!-- <component :is="currentTab"></component> -->
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  data(){
+    return {
+      tabs:[
+        {label:'店铺信息',component:'smzt'},
+        {label:'可售仓库',component:'kdfh'},
+        {label:'退货/维权设置',component:'jysz'}
+      ],
+      currentTab:'smzt'
+    }
+  },
+  components:{
+    
+  }
+}
+</script>
+<style lang="less" scoped>
+  .set-body{
+    height: ~"calc(100% - 47px)";
+    >div{height: 100%;}
+  }
+</style>
